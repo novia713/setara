9a7a3fa14a5b543872824fc5b3d5c2ff7dc27e1a
leandro <leandro@leandro.org>
2016-12-11 17:08:16 +0100

    added emojis support

--- src/Setara.php
+++ src/Setara.php
@@ -13,15 +13,26 @@ namespace Novia713\Setara;
 class Setara
 {
     private $n;
+    private $emojis;
+    private $type;
+
     /**
      * Setara constructor.
      * @param $seed - STRING - name of a 1st generation pokemon
      */
-    public function __construct($seed = null)
+    public function __construct($seed = null, $type = null)
     {
         require __DIR__ . "/../inc/kanto.php";
+        $this->type = $type;
 
         if ($n = (int) array_search($seed, $kanto)) {
+            $this->emojis = array_values(
+              get_object_vars(
+                json_decode(
+                  file_get_contents(__DIR__ . "/../inc/emojis.json")
+                )
+              )
+            );
             $this->n = $n;
         } else {
             die("Please, provide a 1st Pokemon name as seed");
@@ -38,11 +49,15 @@ class Setara
         $i = 0;
         $tmp = [];
         foreach ($arr = str_split($text) as $letter) {
+          if ($this->type == "emojis") {
+            $tmp[] = trim(get_object_vars($this->emojis[ord($letter) + $this->n])['char']);
+          }else{
             if ($i == count($arr) -1) {
                 $tmp[]= ord($letter) + $this->n;
             } else {
                 $tmp[]= ord($letter) + $this->n . ".";
             }
+          }
             $i++;
         }
 
@@ -57,10 +72,36 @@ class Setara
     public function dec($text)
     {
         $tmp = [];
-        foreach (explode(".", $text) as $letter) {
-            $tmp[] = chr($letter - $this->n);
+        if ($this->type == "emojis") {
+          foreach ($arr = $this->mbstring_to_array($text) as $letter) {
+            if ($n = (int) array_search($letter, array_column($this->get_ar_emojis(), 'char'))) {
+              $tmp[] = chr($n - $this->n);
+            }
+          }
+        }else{
+          foreach (explode(".", $text) as $letter) {
+              $tmp[] = chr($letter - $this->n);
+          }
         }
 
         return implode($tmp);
     }
+
+    private function get_ar_emojis() {
+      $_ = [];
+      foreach ($this->emojis as $emoji) {
+        $_[] = get_object_vars($emoji);
+      }
+      return $_;
+    }
+
+    function mbstring_to_array ($string) {
+      $strlen = mb_strlen($string);
+      while ($strlen) {
+          $array[] = mb_substr($string, 0, 1, "UTF-8");
+          $string  = mb_substr($string, 1, $strlen, "UTF-8");
+          $strlen  = mb_strlen($string);
+      }
+      return $array;
+  }
 }
--- test.php
+++ test.php
@@ -4,13 +4,23 @@ require __DIR__ . "/vendor/autoload.php";
 use Novia713\Setara\Setara;
 
 
-$setara = new Setara("Lapras");
+$setara = new Setara("Bulbasaur", "emojis");
+
 $enc = $setara->enc("La guitarra tocada por gitanos, que luego regresaroñ al campo");
 d($enc);
 
 $setara->dec(
   $enc
   );
+
+
+$enc = $setara->enc("610687590 hola");
+d($enc);
+
+d( $setara->dec(
+  $enc
+  ));
+
 
 $enc = $setara->enc("Goran Bregovic compone música para muchos artistas!! ·%&");
 d($enc);
